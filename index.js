a2eed86f06cde04fa338fa67c2466ae5c60f93c7
Gabriel Ting <gabrielfting@gmail.com>
2019-10-06 00:18:17 -0500

    Updated documentation with demo

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { App } = require("@slack/bolt");
 
+// creates an App object using bolt
 const app = new App({
 	token: process.env.SLACK_BOT_TOKEN,
 	signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-// Listens to incoming messages that contain "hello"
+// Listens to incoming messages that contain "hello warhol"
 app.message("hello warhol", ({ message, say }) => {
 	// say() sends a message to the channel where the event was triggered
 	say({
@@ -22,13 +23,14 @@ app.message("hello warhol", ({ message, say }) => {
 						type: "plain_text",
 						text: "Click Me"
 					},
-					action_id: "button_click"
+					action_id: "button_click" // sends an action with an action_id
 				}
 			}
 		]
 	});
 });
 
+// takes the button_click action_id from the "hello warhol" message and responds with a message to the channel
 app.action("button_click", ({ body, ack, say }) => {
 	// Acknowledge the action
 	ack();
@@ -38,8 +40,7 @@ app.action("button_click", ({ body, ack, say }) => {
 // Listens for the warhol slash command
 app.command("/warhol", async ({ payload, command, ack, say }) => {
 	ack();
-	console.log(payload);
-	//say() sends a message to the channel where the event was triggered
+	// uses the slack webAPI to post an ephemeral message–only visible to the sender on slash command invocation
 	let result = await app.client.chat
 		.postEphemeral({
 			token: process.env.SLACK_BOT_TOKEN,
@@ -136,125 +137,25 @@ app.command("/warhol", async ({ payload, command, ack, say }) => {
 		})
 		.then(response => {
 			if (response.ok) {
-				ack();
+				ack(); // sends an acknowledgement after promise resolution
 				console.log("Success!");
 			}
 		})
 		.catch(error => {
-			ack();
+			ack(); // sends an acknowledgement after promise resolution
 			console.log(error);
 		});
-
-	// say({
-	// 	blocks: [
-	// 		{
-	// 			response_type: "ephemeral",
-	// 			type: "section",
-	// 			text: {
-	// 				type: "mrkdwn",
-	// 				text:
-	// 					"Hello, Ruby! *What message would you like to send?*\n\n *Please select a restaurant:*"
-	// 			}
-	// 		},
-	// 		{
-	// 			type: "divider"
-	// 		},
-	// 		{
-	// 			type: "section",
-	// 			text: {
-	// 				type: "mrkdwn",
-	// 				text:
-	// 					"*Farmhouse Thai Cuisine*\n:star::star::star::star: 1528 reviews\n They do have some vegan options, like the roti and curry, plus they have a ton of salad stuff and noodles can be ordered without meat!! They have something for everyone here"
-	// 			}
-	// 		},
-	// 		{
-	// 			type: "section",
-	// 			text: {
-	// 				type: "mrkdwn",
-	// 				text:
-	// 					"*Kin Khao*\n:star::star::star::star: 1638 reviews\n The sticky rice also goes wonderfully with the caramelized pork belly, which is absolutely melt-in-your-mouth and so soft."
-	// 			}
-	// 		},
-	// 		{
-	// 			type: "section",
-	// 			text: {
-	// 				type: "mrkdwn",
-	// 				text:
-	// 					"*Ler Ros*\n:star::star::star::star: 2082 reviews\n I would really recommend the  Yum Koh Moo Yang - Spicy lime dressing and roasted quick marinated pork shoulder, basil leaves, chili & rice powder."
-	// 			}
-	// 		},
-	// 		{
-	// 			type: "divider"
-	// 		},
-	// 		{
-	// 			type: "actions",
-	// 			block_id: "send_button",
-	// 			elements: [
-	// 				{
-	// 					type: "button",
-	// 					text: {
-	// 						type: "plain_text",
-	// 						text: "Farmhouse",
-	// 						emoji: true
-	// 					},
-	// 					value:
-	// 						"They do have some vegan options, like the roti and curry, plus they have a ton of salad stuff and noodles can be ordered without meat!! They have something for everyone here"
-	// 				},
-	// 				{
-	// 					type: "button",
-	// 					text: {
-	// 						type: "plain_text",
-	// 						text: "Kin Khao",
-	// 						emoji: true
-	// 					},
-	// 					value:
-	// 						"The sticky rice also goes wonderfully with the caramelized pork belly, which is absolutely melt-in-your-mouth and so soft."
-	// 				},
-	// 				{
-	// 					type: "button",
-	// 					text: {
-	// 						type: "plain_text",
-	// 						text: "Ler Ros",
-	// 						emoji: true
-	// 					},
-	// 					value:
-	// 						"I would really recommend the  Yum Koh Moo Yang - Spicy lime dressing and roasted quick marinated pork shoulder, basil leaves, chili & rice powder."
-	// 				}
-	// 			]
-	// 		},
-	// 		{
-	// 			type: "divider"
-	// 		},
-	// 		{
-	// 			type: "section",
-	// 			text: {
-	// 				type: "mrkdwn",
-	// 				text:
-	// 					"This would be a message text that you can send if this is clicked"
-	// 			},
-	// 			accessory: {
-	// 				type: "button",
-	// 				text: {
-	// 					type: "plain_text",
-	// 					text: "Send me!",
-	// 					emoji: true
-	// 				},
-	// 				value: "click_me_123"
-	// 			}
-	// 		}
-	// 	]
-	// });
 });
 
 // middleware will only be called when block_id = "send_button"
 app.action({ block_id: "send_button" }, async ({ action, ack, body }) => {
 	ack();
-	// console.log(action);
-	// console.log(context);
-	// console.log(payload);
-	// console.log(action.value);
-	// console.log(action.channel);
-	// console.log(message);
+	console.log(action);
+	console.log(context);
+	console.log(payload);
+	console.log(action.value);
+	console.log(action.channel);
+	console.log(message);
 	try {
 		await app.client.chat
 			.postMessage({
@@ -263,14 +164,21 @@ app.action({ block_id: "send_button" }, async ({ action, ack, body }) => {
 				as_user: true,
 				text: action.value
 			})
-			.then(response => console.log("response succeeded"))
-			.catch(error => console.log(error));
-		//return say(action.value);
+			.then(response => {
+				ack();
+				console.log("response succeeded");
+			})
+			.catch(error => {
+				ack();
+				console.log(error);
+			});
+		//return say(action.value); - alternate method of sending the message as just a bot user
 	} catch (error) {
 		console.error(error);
 	}
 });
 
+// handles actions sent from a direct message text
 app.action(
 	{ block_id: "direct_send" },
 	async ({ action, ack, context, say, payload }) => {
@@ -294,12 +202,6 @@ app.action(
 		}
 	}
 );
-
-// app.action("button_click", ({ body, ack, say }) => {
-// 	// Acknowledge the action
-// 	ack();
-// 	say(`<@${body.user.id}> clicked the button`);
-// });
 
 (async () => {
 	// Start your app
